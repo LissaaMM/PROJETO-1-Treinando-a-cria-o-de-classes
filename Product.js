19c6be081049151a0d8dce57a85cf4de425d5fdb
Ana Larissa Mendes <152804250+LissaaMM@users.noreply.github.com>
2024-08-09 08:07:18 -0300

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,34 +1,30 @@
 class Product {
     constructor(name, description, price) {
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.inStock = 0;
+        this.name = name
+        this.description = description
+        this.price = price
+        this.inStock = 0
     }
 
-    addToStock(quantity) {
-        this.inStock += quantity;
+    addToStock(quantility) {
+        this.inStock += quantility
     }
 
     calculateDiscount(discountPercentage) {
-        const discountAmount = this.price * (discountPercentage / 100);
-        return this.price - discountAmount;
+        const discountQuantia = this.price * (discountPercentage / 100)
+        return this.price - discountQuantia
     }
 }
 
-module.exports = Product;
+const produto = new Product("Notebook", "Notebook Acer Aspire 5", 2.916.97)
+produto.addToStock(10)
 
-const Product = require('./product');
-
-const produto = new Product("Notebook", "Notebook Nitro 5", 1000.00);
-produto.addToStock(10);
-
-console.log(`Nome do produto: ${produto.name}`);
-console.log(`Descrição do produto: ${produto.description}`);
-console.log(`Valor do produto: R$${produto.price.toFixed(2)}`);
-console.log(`Quantidade no estoque: ${produto.inStock}`);
-console.log("==================================");
-const desconto = 5;
-const novoValor = produto.calculateDiscount(desconto);
-console.log(`Desconto no produto: ${desconto}%`);
-console.log(`Novo valor do produto: R$${novoValor.toFixed(2)}`);
+console.log(`Nome do produto: ${produto.name}`)
+console.log(`Descrição do produto: ${produto.description}`)
+console.log(`Valor do produto: R$${produto.price.toFixed(2)}`)
+console.log(`Quantidade no estoque: ${produto.inStock}`)
+console.log("==================================")
+const desconto = 5
+const novoValor = produto.calculateDiscount(desconto)
+console.log(`Desconto no produto: ${desconto}%`)
+console.log(`Novo valor do produto: R$${novoValor.toFixed(2)}`)
